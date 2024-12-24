58d74d542296583403a0df48e2ffb20718557a56
LarryEmerson12 <158800143+LarryEmerson12@users.noreply.github.com>
2024-12-24 12:02:48 +0800

    fix code errors

--- src/app/answers/page.js
+++ src/app/answers/page.js
@@ -51,7 +51,7 @@ export default function Answers() {
         <>
           <button
             onClick={
-              setCount(count + 1);
+              setCount(count + 1)
             }
           >
             Count is {count}
@@ -66,7 +66,7 @@ export default function Answers() {
   const code1f = `
     <button
       onClick={
-        setCount(count + 1);
+        setCount(count + 1)
       }
     >
       Count is {count}
@@ -85,7 +85,7 @@ export default function Answers() {
         <>
           <button
             onClick={
-              setCount(count + 1);
+              setCount(count + 1)
             }
           >
             Count is {count}
